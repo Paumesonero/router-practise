9d2e1da28ef590c2412834573be272baaaa3363b
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2022-08-18 19:44:32 +0200

    practice done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import Home from './views/Home';
+import Navbar from './components/Navbar';
+import Contact from './views/Contact';
+import Projects from './views/Projects';
+import ProjectsGames from './components/ProjectsGames';
+import ProjectsApp from './components/ProjectsApps';
+import ErrorPage from './views/ErrorPage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar/>
+      <Routes>
+        <Route path={'/'} element={<Home/>} />
+        <Route path={'/contact'} element={<Contact/>} />
+        <Route path = {'/projects'} element={<Projects/>}>
+          <Route path={'games'} element={<ProjectsGames/>} />
+          <Route path={'apps'} element={<ProjectsApp />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      
     </div>
   );
 }
--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
 export default function Projects() {
   return (
     <div>
       <h1>My projects</h1>
       <p>I have separated my projects into two categories: games I have designed and developed, and applications I have programmed.</p>
+      <Link to={'/projects/games'}>See Games</Link>
+      <Link to={'/projects/apps'} >See Apps</Link>
+      <Outlet />
     </div>
   )
 }
